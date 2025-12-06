b8c599cd0762cce5d050e048e65443c06949ed66
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-06 17:13:18 +0530

    feat: Modified theme_manager.hpp

---
+++ include/theme_manager.hpp
@@ -0,0 +1,12 @@
+//theme_manager.cpp header
+#ifndef THEME_MANAGER_H
+#define THEME_MANAGER_H
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+
+void changeTheme(std::string theme);
+
+#endif
