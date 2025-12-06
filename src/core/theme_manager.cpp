50962fcec6dd60de82bd8d91e0329a43ee329e76
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-06 17:12:52 +0530

    feat: Modified theme_manager.cpp

---
+++ src/core/theme_manager.cpp
@@ -0,0 +1,20 @@
+#include "../../include/theme_manager.hpp"
+
+void changeTheme(std::string theme){ //for command-line based
+    //std::string themeType;
+    if(theme=="light"){
+        system("color 70");//white background with black text
+    }
+    else if(theme=="dark"){
+        system("color 07"); // black backgound with white text / default theme
+    }
+    else
+        std::cout<<"Invalid theme !\n";
+}
+
+void changeTheme(){  //for interactive mode (menu based)
+    std::string themeType;
+    std::cout<<"Enter theme: (dark / light) :";
+    std::cin>>themeType;
+    changeTheme(themeType);
+}
