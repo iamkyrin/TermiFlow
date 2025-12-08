1af4146c81d88be1c52ffd4a5e245dd5ecb1c68b
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-08 18:52:39 +0530

    feat: Modified shortcuts.cpp file

--- src/features/shortcuts.cpp
+++ src/features/shortcuts.cpp
@@ -1,2 +1,20 @@
 //shortcuts.cpp file: for creating and managing user-defined shortcuts (for launch commands as of now)
+#include <iostream>
+#include <fstream>
 #include "../../include/shortcuts.hpp"
+
+void shortcuts::add(const std::string& def, const std::string& unew){
+
+}
+
+void shortcuts::remove(const std::string& def){
+
+}
+void shortcuts::save(){
+
+}
+
+void shortcuts::list(){
+    
+}
+
