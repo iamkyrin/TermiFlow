499df76af5ded17e322b3f3aba970adc915459c7
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-08 18:59:32 +0530

    feat: Modified shortcuts.cpp file

--- src/features/shortcuts.cpp
+++ src/features/shortcuts.cpp
@@ -3,18 +3,24 @@
 #include <fstream>
 #include "../../include/shortcuts.hpp"
 
-void shortcuts::add(const std::string& def, const std::string& unew){
-
+void shortcuts::add(const std::string& key, const std::string& value){
+    shortMap[key]=value;
+    save();
+    std::cout<<"Yeah !. Shortcut added: "<<key<<" -> "<<value<<"\n";
 }
 
-void shortcuts::remove(const std::string& def){
+void shortcuts::remove(const std::string& value){
 
 }
 void shortcuts::save(){
-
+    std::ofstream outfile(filepath);
+    for(auto& p : shortMap) {
+        outfile << p.first << "=" << p.second << "\n";
+    }
+    outfile.close()
 }
 
 void shortcuts::list(){
-    
+
 }
 
