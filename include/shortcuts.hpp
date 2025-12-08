7349625aec022c670d09d6f8b4394716d0514b94
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-08 18:47:33 +0530

    feat: Modified shortcuts.hpp file

---
+++ include/shortcuts.hpp
@@ -0,0 +1,22 @@
+//header file for shortcuts.cpp 
+#ifndef SHORTCUTS
+#define SHORTCUTS
+
+#include <string>
+#include <sstream>
+#include <unordered_map>
+
+class shortcuts {
+    private:
+    std::unordered_map<std::string, std::string> shortMap;
+    std::string filepath;
+
+    public:
+    void add(const std::string& def, const std::string& unew ); //def: default key; unew: user's new key
+    void remove(const std::string& def);
+    void save();
+    void list();
+
+}
+
+#endif
