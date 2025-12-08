7c1eeb38bfd4cca6769fc6cbcfc4792291fb8480
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-08 19:05:19 +0530

    feat: Modified shortcuts.cpp file

--- src/features/shortcuts.cpp
+++ src/features/shortcuts.cpp
@@ -4,28 +4,28 @@
 #include "../../include/shortcuts.hpp"
 
 void shortcuts::add(const std::string& key, const std::string& value){
-    shortMap[key]=value;
+    shortMap[key]=value; //assigning value to key(the default command word)
     save();
     std::cout<<"Yeah !. Shortcut added: "<<key<<" -> "<<value<<"\n";
 }
 
 void shortcuts::remove(const std::string& key){
-    shortMap.erase(key);
+    shortMap.erase(key); //erase() : erases the 'key' passed to it from the unordered map.
     save();
     std::cout<<"Shortcut for "<<key<<" removed!.\n";
 
 }
 void shortcuts::save(){
     std::ofstream outfile(filepath);
-    for(auto& p : shortMap) {
+    for(auto& p : shortMap) { //for each loop
         outfile << p.first << "=" << p.second << "\n";
     }
-    outfile.close()
+    outfile.close();
 }
 
 void shortcuts::list(){
     std::cout << "Registered shortcuts:\n";
-    for(auto& p : ap) {
+    for(auto& p : shortMap) { //for each loop
         std::cout << " - " << p.first << " -> " << p.second << "\n";
     }
 
