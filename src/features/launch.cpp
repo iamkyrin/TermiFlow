28d710f70a204f6da38cc0c525a933c403532fa8
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-10 19:17:57 +0530

    feat: Modified launch.cpp file

--- src/features/launch.cpp
+++ src/features/launch.cpp
@@ -9,22 +9,7 @@ void launchApp() {
     std::string appName;
     std::cout<<">>>launch "; 
     std::cin>>appName;
-
-    if (appName == "chrome") {
-        command = "start chrome";
-    }
-    else if (appName == "vscode") {
-        command = "code";
-    }
-    else if (appName == "notepad") {
-        command = "notepad";
-    }
-    else {
-        std::cout << "Unknown app: " << appName << "\n";
-        return;
-    }
-    system(command.c_str());
-    std::cout << "Launching " << appName << "...\n";
+    launchApp(appName);
 }
 
 void launchApp(std::string appName) {
