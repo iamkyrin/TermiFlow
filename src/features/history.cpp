377b766092c03edbcc127f6de58ed5f41bdbd7b5
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-11 19:18:16 +0530

    feat: Modified history.cpp file

--- src/features/history.cpp
+++ src/features/history.cpp
@@ -1,1 +1,21 @@
 //history.cpp: handles operation of saving command_handler command history
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "../../include/history.hpp"
+
+
+
+void history::load() {
+    cmdEntries.clear();
+    std::ifstream infile(filePath);
+    if(!infile.is_open()){
+        return;
+    }
+
+    std::string line;
+    while(std::getline(infile, line)) {
+        cmdEntries.push_back(line);
+    }
+    infile.close();
+}
