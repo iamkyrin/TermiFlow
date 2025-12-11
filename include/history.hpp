bc8d4bd14fe6f4b3897311bf8900f64b40839b26
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-11 19:09:31 +0530

    feat: Modified history.hpp file

---
+++ include/history.hpp
@@ -0,0 +1,20 @@
+
+
+#include <string>
+#include <vector>
+
+class History {
+public:
+    History(const std::string& path);
+
+    void load();
+    void save();
+
+    void add(const std::string& cmd);
+    void list() const;
+    std::string get(int index) const;
+
+private:
+    std::string filePath;
+    std::vector<std::string> entries;
+};
