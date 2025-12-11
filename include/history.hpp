dabc3448a090c87bad102e6a2737022b2cf44fe9
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-11 19:12:28 +0530

    feat: Modified history.hpp file

--- include/history.hpp
+++ include/history.hpp
@@ -3,18 +3,16 @@
 #include <string>
 #include <vector>
 
-class History {
+class history {
 public:
-    History(const std::string& path);
-
-    void load();
-    void save();
-
-    void add(const std::string& cmd);
-    void list() const;
-    std::string get(int index) const;
+    void load(); //load commands
+    void save(); //save commands
+    history();
+    void add(const std::string& cmd); //add command to history
+    void list(); //list the past commands
+    std::string get(int index);
 
 private:
     std::string filePath;
-    std::vector<std::string> entries;
+    std::vector<std::string> cmdEntries;
 };
