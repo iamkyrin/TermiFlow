d7ea57cb82fa3aa9680295f8d721b074339ff98e
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-16 19:28:50 +0530

    feat: Modified system_stats.cpp file

---
+++ src/core/system_stats.cpp
@@ -0,0 +1,9 @@
+//system stats such as CPU usage, Memory usage, and Uptime is displayed using this file
+#include "../../include/system_stats.hpp"
+#include <windows.h>
+#include <sstream>
+
+std::string SystemStats::getCPU_Usage() {
+    // Placeholder for now
+    return "CPU: calculating...";
+}
