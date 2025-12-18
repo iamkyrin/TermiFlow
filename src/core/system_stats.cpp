fc11f10e4ad0e8f950927659e56a4dfb7d10443e
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-18 21:05:04 +0530

    feat: Added getMemoryUsage() function to system_stats.cpp file

--- src/core/system_stats.cpp
+++ src/core/system_stats.cpp
@@ -8,3 +8,16 @@ std::string SystemStats::getCPU_Usage() {
     return "CPU: calculating...";
 }
 //Add memory usage function, uptime finctiom and complete the cpu usage function.
+
+std::string SystemStats::getMemoryUsage() { //contains builtin and system level functions and keywords
+    MEMORYSTATUSEX memInfo;
+    memInfo.dwLength = sizeof(memInfo);
+    GlobalMemoryStatusEx(&memInfo);
+
+    long long total = memInfo.ullTotalPhys / (1024 * 1024);
+    long long used  = (memInfo.ullTotalPhys - memInfo.ullAvailPhys) / (1024 * 1024);
+
+    std::ostringstream out;
+    out << "Memory: " << used << " / " << total << " MB\n"<<(used/total)*100.0<<"%";
+    return out.str();
+}
