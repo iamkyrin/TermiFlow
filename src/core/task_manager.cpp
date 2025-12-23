9154a5a7df5da48fadc8f139813691a85d527234
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-23 15:19:14 +0000

    feat: added header file

--- src/core/task_manager.cpp
+++ src/core/task_manager.cpp
@@ -1,5 +1,5 @@
 // Will be implemented in the upcoming version releases.
-
+#include "../../include/task_manager.hpp"
 
 void taskManager(){
     std::cout<<">> Not available for the current version. <<\n";
