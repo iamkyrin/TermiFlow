6e3f00a1792c21bee68af0ba89726168211855a7
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-23 15:18:49 +0000

    feat: added header file

--- src/features/session_manager.cpp
+++ src/features/session_manager.cpp
@@ -1,4 +1,5 @@
 // Will be implemented in the upcoming version releases.
+#include "../../include/session_manager.hpp"
 
 void sessionManager(){
     std::cout<<">> Not available for the current version. <<\n";
