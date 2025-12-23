4c99de00a42148a466d17c3c2a62e4622b0f7ef3
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-12-23 13:53:08 +0000

    feat: added display statement for N/A

--- src/core/config_loader.cpp
+++ src/core/config_loader.cpp
@@ -1,1 +1,6 @@
 // Will be implemented in the upcoming version releases.
+#include "../../include/config_loader.hpp"
+
+void configLoader(){
+    std::cout<<">> Not available for the current version. <<\n";
+}
