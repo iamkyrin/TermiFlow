9eb00ccb2034021affb9a8038f1f7fbc81248b76
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-01-30 10:24:09 +0000

    feat: added linux systems stat generation functionality

--- src/core/system_stats.cpp
+++ src/core/system_stats.cpp
@@ -1,16 +1,24 @@
 //system stats such as CPU usage, Memory usage, and Uptime is displayed using this file
 //Check the entire flow of the program and understand it and test for various test cases.
 #include "../../include/system_stats.hpp"
-#include <windows.h>
 #include <sstream>
 #include <iostream>
+
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+    #include <sys/sysinfo.h>
+    #include <fstream>
+#endif
 
 std::string SystemStats::getCPU_Usage() {
     // Placeholder for now (fix this after beta release)
     return "CPU: N/A";
 }
 
-std::string SystemStats::getMemoryUsage() { //contains built-in and system level functions and keywords
+std::string SystemStats::getMemoryUsage() {
+#ifdef _WIN32
     MEMORYSTATUSEX memInfo;
     memInfo.dwLength = sizeof(memInfo);
     GlobalMemoryStatusEx(&memInfo);
@@ -18,12 +26,24 @@ std::string SystemStats::getMemoryUsage() { //contains built-in and system level
     long long total = memInfo.ullTotalPhys / (1024 * 1024);
     long long used  = (memInfo.ullTotalPhys - memInfo.ullAvailPhys) / (1024 * 1024);
 
-    std::ostringstream out; //to return the whole coutput string as a return string
-    out << "Memory: " << used << " / " << total << " MB\t [" <<(used/float(total))*100<<"% ]"; //this is a string which is actually an output
-    return out.str();//returns the output string mentioned above
+    std::ostringstream out;
+    out << "Memory: " << used << " / " << total << " MB\t [" << (used/float(total))*100 << "% ]";
+    return out.str();
+#else
+    struct sysinfo info;
+    sysinfo(&info);
+    
+    long long total = info.totalram / (1024 * 1024);
+    long long used = (info.totalram - info.freeram) / (1024 * 1024);
+    
+    std::ostringstream out;
+    out << "Memory: " << used << " / " << total << " MB\t [" << (used/float(total))*100 << "% ]";
+    return out.str();
+#endif
 }
- //function for getting and calculating uptime in seconds, minutes and hours.
+
 std::string SystemStats::getUptime() {
+#ifdef _WIN32
     DWORD uptimeMs = GetTickCount();
     int seconds = uptimeMs / 1000.0;
     int minutes = seconds / 60.0;
@@ -32,6 +52,18 @@ std::string SystemStats::getUptime() {
     std::ostringstream outputString;
     outputString << "Uptime: " << hours << "h " << minutes << "m";
     return outputString.str();
+#else
+    struct sysinfo info;
+    sysinfo(&info);
+    
+    long uptime_seconds = info.uptime;
+    int hours = uptime_seconds / 3600;
+    int minutes = (uptime_seconds % 3600) / 60;
+    
+    std::ostringstream outputString;
+    outputString << "Uptime: " << hours << "h " << minutes << "m";
+    return outputString.str();
+#endif
 }
 
 //fuction which displays all of the three details
