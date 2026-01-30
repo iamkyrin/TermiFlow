855ae584cd49c1913601ee4eff48fd697c70d0e0
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-01-30 10:14:15 +0000

    feat: added config save functionality

--- src/core/theme_manager.cpp
+++ src/core/theme_manager.cpp
@@ -3,14 +3,18 @@
 
 void changeTheme(std::string theme){ //for command handler mode
     //std::string themeType;
+    saveThemeToConfig(theme);
     if(theme=="light"){
-        system("color 70");//white background with black text
+        system("clear");
+        std::cout << "\033[47;30m"; // white background with black text
     }
     else if(theme=="dark"){
-        system("color 07"); //black backgound with white text / default theme
+        system("clear");
+        std::cout << "\033[40;37m"; // black background with white text / default theme
     }
     else
         std::cout<<"Invalid theme !\n";
+    
 }
 
 void changeTheme(){  //for interactive mode (menu based)
@@ -18,4 +22,20 @@ void changeTheme(){  //for interactive mode (menu based)
     std::cout<<"Enter theme: (dark / light) :";
     std::cin>>themeType;
     changeTheme(themeType);
+}
+
+void saveThemeToConfig(const std::string& theme) {
+    std::ofstream file("config/termiflow.conf");
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open config file for writing!\n";
+        return;
+    }
+    file <<
+        "[user_interface]\n"
+        "theme=" << theme << "\n"
+        "show_banner=true\n\n"
+        "[behavior]\n"
+        "auto_apply_theme=true\n";
+    file.close();
+    std::cout << "Theme saved successfully!\n";
 }
